3c8c40f35f53dc42e212dd7749cd8e1388dccb5c
Terry Harmer <terry.harmer@gmail.com>
2015-08-25 22:09:05 -0400

    Enumerators 2

--- fluer_status.rb
+++ fluer_status.rb
@@ -20,14 +20,41 @@ flyers << Flyer.new("Moe", "moe@example.com", 1000)
 flyers << Flyer.new("Curly", "curly@example.com", 3000, :gold)
 flyers << Flyer.new("Shemp", "shemp@example.com", 2000)
 
-freq_flyers = flyers.select {|f| f.miles_flown >= 3000}
-puts freq_flyers
-puts
-infreq_flyers = flyers.reject {|f| f.miles_flown >= 3000}
-puts infreq_flyers
-puts
-plat_flyers = flyers.any? {|f| f.status = :platinum}
-puts plat_flyers
-puts
-bronz_flyers = flyers.detect {|f| f.status == :bronze}
-puts bronz_flyers
+
+distance = flyers.map { |f| f.miles_flown * 1.6 }
+p distance
+p distance.reduce(:+)
+              
+total_miles_flown = flyers.reduce(0) { |sum, flyer| sum + flyer.miles_flown }
+puts "Total miles flown: #{total_miles_flown}"
+
+total_kms_flown = flyers.map { |flyer| flyer.miles_flown * 1.6 }.reduce(0, :+)
+puts "Total km flown: #{total_kms_flown}"
+
+bronze_total = flyers.select { |f| f.status == :bronze }.map { |f| f.miles_flown * 1.6 }.reduce(:+)
+puts bronze_total
+
+max_flyer = flyers.max_by {|f| f.miles_flown}
+puts max_flyer
+min_flyer = flyers.min_by {|f| f.miles_flown}
+puts min_flyer
+#platinum, coach = flyers.partition {|f| f.status == :platinum}
+#puts "Platinum:"
+#puts platinum
+#puts
+#puts "Coach:"
+#puts coach
+#puts 
+#nametag = flyers.map { |f| "#{f.name} (#{f.status.upcase})" }
+#p nametag
+#freq_flyers = flyers.select {|f| f.miles_flown >= 3000}
+#puts freq_flyers
+#puts
+#infreq_flyers = flyers.reject {|f| f.miles_flown >= 3000}
+#puts infreq_flyers
+#puts
+#plat_flyers = flyers.any? {|f| f.status = :platinum}
+#puts plat_flyers
+#puts
+#bronz_flyers = flyers.detect {|f| f.status == :bronze}
+#puts bronz_flyers
--- scores.rb
+++ scores.rb
@@ -1,11 +1,23 @@
 scores = [83, 71, 92, 64, 98, 87, 75, 69]
 
-high_scores = scores.select{|s|  s > 80 }
-puts high_scores
+scores_doubled = scores.map { |s| s * 2 }
+p scores_doubled
+puts
+p scores.reduce(:+)
+puts
+evens, odds = scores.partition { |s| s % 2 == 0 }
+p evens
+p odds
+puts 
+evens, odds = scores.partition { |score| score.even? }
+puts "Evens:"
+p evens
+puts "Odds:"
+p odds
 
-low_scores = scores.reject{|s|  s > 80 }
-puts low_scores
-
-puts scores.any?{|s|  s < 70 }
-
-puts scores.detect{|s|  s < 70 }
+#high_scores = scores.select{|s|  s > 80 }
+#puts high_scores
+#low_scores = scores.reject{|s|  s > 80 }
+#puts low_scores
+#puts scores.any?{|s|  s < 70 }
+#puts scores.detect{|s|  s < 70 }
